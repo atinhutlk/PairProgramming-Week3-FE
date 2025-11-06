d188c6866f105ef59445a717ab74a64aad72ba16
atinhutlk <nhut.vo@metropolia.fi>
2025-11-06 11:24:55 +0200

    Hero update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,20 @@
+import React from "react";
+import styles from "../Hero.module.css"; 
+
 function Hero() {
   return (
-   <section className="hero" id="home">
-      <div className="hero-banner">
+    <section className={styles.hero} id="home">
+      <div className={styles["hero-banner"]}>
         <h1>backroads app</h1>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae explicabo
-          debitis est autem dicta.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
+          explicabo debitis est autem dicta.
         </p>
-        <a href="#tours" className="btn hero-btn">
-          explore tours
+        <a href="#tours" className={styles["hero-btn"]}>
+          Explore tours
         </a>
       </div>
     </section>
-
   );
 }
 
